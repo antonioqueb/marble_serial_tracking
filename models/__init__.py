5c16b98194e27483d53b717f858920e6ae2a3993
Antonio Queb <antonioqueb@alphaqueb.online>
2025-04-05 21:51:34 -0600

    Up

--- models/__init__.py
+++ models/__init__.py
@@ -5,3 +5,4 @@ from . import stock_lot
 from . import stock_move
 from . import purchase_order
 from . import sale_order_line
+from . import stock_rule
---
+++ models/stock_rule.py
@@ -0,0 +1,25 @@
+# models/stock_rule.py
+
+from odoo import models, fields
+
+class StockRule(models.Model):
+    _inherit = 'stock.rule'
+
+    def _get_stock_move_values(self, product_id, product_uom, location_id, name,
+                               origin, company_id, values):
+        """
+        values es el diccionario que viene de _prepare_procurement_values() 
+        en sale.order.line. Aquí tomamos esos campos y los inyectamos en 
+        el diccionario que acabará creando un stock.move.
+        """
+        res = super()._get_stock_move_values(product_id, product_uom, location_id, 
+                                             name, origin, company_id, values)
+        # Copiamos tus campos personalizados:
+        res.update({
+            'marble_height': values.get('marble_height', 0.0),
+            'marble_width': values.get('marble_width', 0.0),
+            'marble_sqm': values.get('marble_sqm', 0.0),
+            'lot_general': values.get('lot_general', ''),
+            'lot_id': values.get('lot_id'),
+        })
+        return res
