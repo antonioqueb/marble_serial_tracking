0352795ea7faf9f35a134e6f4ebfee2783771ab3
Antonio Queb <cardstillo@gmail.com>
2025-05-24 14:54:02 -0600

    Up

--- models/__init__.py
+++ models/__init__.py
@@ -6,6 +6,5 @@ from . import stock_move
 from . import purchase_order
 from . import sale_order_line
 from . import stock_rule
-from . import stock_picking
 from . import product_template
 from . import sale_order_line_pricing
